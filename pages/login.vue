9a4c4fe75c6d789bc72d36985dad015f9558ebd1
akletini <allen.k@freenet.de>
2023-02-03 21:32:15 +0100

    Functional login page for Google

--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,4 @@
+import mySignInHandler from '../server/api/auth/signin[]';
 <template>
   <div>
     <section class="bg-gray-50 dark:bg-gray-900">
@@ -79,13 +80,16 @@
               </div>
               <button
                 type="submit"
-                class="w-full text-white border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full text-white border bg-primary-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
               </button>
               <button
                 type="submit"
-                @click="signIn('google', { callbackUrl: '/' })"
+                @click="
+                  () =>
+                    signIn('google', { callbackUrl: 'http://localhost:3000/' })
+                "
                 class="flex flex-row justify-center w-full border items-center text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 <svg
@@ -135,12 +139,15 @@ definePageMeta({
   layout: "empty",
   auth: false,
 });
-const router = useRouter();
-router.push("/login");
-const { status, data, signIn, signOut } = useSession();
-status.value;
-data.value; // Session data, e.g., expiration, user.email, ...
-const statusValue = status.value; // Session status: `unauthenticated`, `loading`, `authenticated`
+const routeName = useRoute().fullPath.toString();
+if (routeName! !== "/login?email=&password=") {
+  useRouter().push("/login?email=&password=");
+}
+const { status, data, signIn } = useSession();
+
+// status.value;
+// data.value; // Session data, e.g., expiration, user.email, ...
+// const statusValue = status.value; // Session status: `unauthenticated`, `loading`, `authenticated`
 </script>
 
 <style scoped></style>
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -3,15 +3,30 @@ import GoogleProvider from "next-auth/providers/google";
 const config = useRuntimeConfig();
 export default NuxtAuthHandler({
   pages: {
-    // Change the default behavior to use `/login` as the path for the sign-in page
-    signIn: "/login",
-    error: "/login",
+    signIn: "/login?email=&password=",
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
   },
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GoogleProvider.default({
       clientId: config.clientId,
       clientSecret: config.clientSecret,
+      authorization: {
+        params: {
+          scope:
+            "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/tasks",
+        },
+      },
     }),
   ],
 });
